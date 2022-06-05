ce4a32a2ff0768aba7d50cac147c22d6eb1b7037
Boris <ea.foreverrr@gmail.com>
2022-06-05 21:36:08 +0300

    Initial commit

---
+++ Day_1_OOP/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Day_1_OOP
+//
+//  Created by test on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        print("Hello App")
+    }
+
+
+}
+
